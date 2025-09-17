e26569352fdb84cd4156a613ee8957108a5c5f92
ChickenTTO <datthutich69@gmail.com>
2025-09-18 00:43:42 +0700

    done cam, login and sign

--- btl-ai/src/App.js
+++ btl-ai/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login/login';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from './pages/signUp/signUp';
+import WebCam from './pages/webCam/webCam';
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<WebCam />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- btl-ai/src/pages/Login/login.jsx
+++ btl-ai/src/pages/Login/login.jsx
@@ -1,59 +1,66 @@
 import React from 'react'
 import './login.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons' 
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faLock } from '@fortawesome/free-solid-svg-icons'
-import {faFacebook} from '@fortawesome/free-brands-svg-icons'
-import {faGoogle} from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { useNavigate } from 'react-router-dom'
 
 
-const login = () => {
+const Login = () => {
+const navigate = useNavigate();
+
   return (
     <div>
       <div className="login">
         <div className="login-backgroud">
 
 
-            <div className="title-login">
-                <p>Login your account</p>
-            </div>
+          <div className="title-login">
+            <p>Login your account</p>
+          </div>
 
 
-            <div className="input">
+          <div className="input">
             <div className="input-acc">
-                <FontAwesomeIcon icon={faUser} className='icon'/>
-                <input type="text" placeholder='User name'/>
+              <FontAwesomeIcon icon={faUser} className='icon' />
+              <input type="text" placeholder='User name' />
             </div>
             <div className="input-pass">
-                <FontAwesomeIcon icon={faLock} className='icon' />
-                <input type="password" placeholder='Password'/>
+              <FontAwesomeIcon icon={faLock} className='icon' />
+              <input type="password" placeholder='Password' />
             </div>
 
-            </div>
+          </div>
 
-            <div className="button">
+          <div className="button">
             <div className="button-login">
-                <button>Login</button>
+              <button>Login</button>
             </div>
             <div className="button-signUp">
-                <button>Sign up</button>
+              <button onClick={() => navigate("/signup")}>Sign up</button>
             </div>
+          </div>
+          <div className="or">
+            <p>Or</p>
+          </div>
+          <div className="face-goo">
+            <div className="face">
+              <button> <a href="https://www.facebook.com/"> <FontAwesomeIcon icon={faFacebook} className='face' /> Sign for Facebook</a> </button>
             </div>
-            <div className="or">
-                <p>Or</p>
+            <div className="google">
+              <button> <a href="https://accounts.google.com/v3/signin/identifier?continue=http%3A%2F%2Fsupport.google.com%2Fmail%2Fanswer%2F56256%3Fhl%3Den&ec=GAZAdQ&hl=en&ifkv=AfYwgwWOkqSvh7vhy_TLIerVhd1jStKc9FA4wS_cB0HT9ECa8I3yxOry08V5sCL3Fn8U4BPDPJCE2A&passive=true&sjid=10322847849808881535-NC&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S121179217%3A1758127040658342"> <FontAwesomeIcon icon={faGoogle} className='google' /> Sign for Google</a> </button>
             </div>
-            <div className="face-goo">
-                <div className="face">
-                <button> <FontAwesomeIcon icon={faFacebook} className='face' /> Sign for Facebook</button>
-                </div>
-                <div className="google">
-                <button> <FontAwesomeIcon icon={faGoogle} className='google'/> Sign for Google</button>
-                </div>
-            </div>
+          </div>
+          <div className="back">
+          <button onClick={() => navigate("/")}>Back to the Webcam</button>
+          </div>
         </div>
       </div>
     </div>
   )
 }
 
-export default login
+
+export default Login
