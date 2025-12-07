d935a0c12d4e5f7000f43ca8dbc84407fd89e248
ChickenTTO <datthutich69@gmail.com>
2025-12-08 00:26:03 +0700

    cam

--- btl-ai/src/pages/webCam/webCam.jsx
+++ btl-ai/src/pages/webCam/webCam.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import Webcam from 'react-webcam'
-import './webCam.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Webcam from "react-webcam";
+import "./webCam.css";
 
 const WebCam = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="webCam">
-            <div className="cam">
-            <h1>Timekeeping camera</h1>
-            <Webcam
-                audio={false}
-                screenshotFormat="image/jpeg"
-                width={350}
-            />
-            </div>
-
-            <div className="bt-login">
-            <div style={{ marginTop: "20px" }}> 
-                <button onClick={() => navigate("/login")}>Login with account Addmin</button>
-            </div>
-
-            </div>
+  return (
+    <div className="webCam">
+      <div className="cam">
+        <h1>Timekeeping camera</h1>
+        <div className="Webcam">
+          <Webcam audio={false} screenshotFormat="image/jpeg" width={350} />
         </div>
-    )
-}
+      </div>
 
+      <div className="bt-login">
+        <div style={{ marginTop: "20px" }}>
+          <button onClick={() => navigate("/login")}>
+            Login with account Addmin
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default WebCam
+export default WebCam;
